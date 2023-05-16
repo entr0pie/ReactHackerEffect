b6a46580841115ffa6fb595160b1e1364c2a7e32
tandera@notemail.com <email@email.com>
2023-05-16 09:57:53 -0300

    Functional version!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const original = "HACKER-EFFECT"; 
+  const time_ms = 30;
+  const iterations_per_shift = 5;
+
+  const [text, setText] = useState(original);
+
+  function getRandomString(length) {
+    const getRandomLetter = () => {
+      const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      return alphabet[randomIndex];
+    }   
+
+    let random = "";  
+    
+    for (var i = 0; i < length; i++) {
+      random += getRandomLetter();     
+    }
+    
+    return random;
+  }
+
+  async function hackerEffect() {
+
+    setText(getRandomString(original.length));
+    await new Promise(r => setTimeout(r, time_ms));
+    
+    var original_copy = "";
+    
+    for (var i = 0; i < original.length; i++) {
+      original_copy += original[i];
+      
+      for (var j = 0; j < iterations_per_shift; j++) {
+        const shift = original_copy + getRandomString(original.length - original_copy.length);
+        setText(shift);
+        await new Promise(r => setTimeout(r, time_ms));
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 onMouseEnter={hackerEffect}>{text}</h1>
     </div>
   );
 }
